06eec458f3917ea18ba0df3084db92eec2669fe4
Hoang <hoang.nt@anlab.vn>
2014-10-27 00:36:48 +0700

    font end

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Romantic::Application.config.secret_token = '628dbcc1df1aa3d0169c7aa75df77a45a6eddadfdbac2d9c323f965704e4ee85db6dc74c97c5e04bd51780e2c804501a64c18b8e7c6bc2fffc126a73cc22cae1'
